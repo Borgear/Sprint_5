12196909e44c093594e0be74ebc9fe4eb7ec86aa
Dmitry Baryshev <d_borg@mail.ru>
2026-02-28 17:27:34 +0300

    1-st commit (generator)

--- generator.py
+++ generator.py
@@ -2,7 +2,7 @@ import random
 
 def generate_unique_email(): #Генерирует email по шаблону: имя_фамилия_номер_когорты_3цифры@домен
     
-    digits = random.randint(1000, 9999)
+    digits = random.randint(100, 999)
     return f"Dmitry_Baryshev_41_{digits}@yandex.ru"
 
 def generate_random_password(length=8): #Генерирует случайный пароль заданной длины
